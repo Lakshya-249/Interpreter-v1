d37bc12ae1e6e63f987fcefd096d83e53d652f3a
dev-lakshya004 <lakshya@factacy.ai>
2026-03-17 21:56:03 +0530

    added a native fucntion

---
+++ src/main/java/com/lakshya/interpreter/runtime/ClockNativeFunction.java
@@ -0,0 +1,21 @@
+package com.lakshya.interpreter.runtime;
+
+import java.util.List;
+
+public class ClockNativeFunction implements Callable {
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double) System.currentTimeMillis() / 1000.0;
+    }
+
+    @Override
+    public String toString() {
+        return "<native fn>";
+    }
+}
--- src/main/java/com/lakshya/interpreter/runtime/Interpreter.java
+++ src/main/java/com/lakshya/interpreter/runtime/Interpreter.java
@@ -14,28 +14,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     private Environment environment = globals;
 
     public Interpreter() {
-        globals.define(
-            "clock",
-            new Callable() {
-                @Override
-                public int arity() {
-                    return 0;
-                }
-
-                @Override
-                public Object call(
-                    Interpreter interpreter,
-                    List<Object> arguments
-                ) {
-                    return (double) System.currentTimeMillis() / 1000.0;
-                }
-
-                @Override
-                public String toString() {
-                    return "<native fn>";
-                }
-            }
-        );
+        globals.define("clock", new ClockNativeFunction());
     }
 
     private Object evaluate(Expr expr) {
